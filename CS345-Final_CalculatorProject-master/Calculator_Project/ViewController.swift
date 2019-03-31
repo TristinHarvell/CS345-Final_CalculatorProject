9088e3fb7e89f19fffa79bbe9d569ee210271b49
anukic17 <46793246+anukic17@users.noreply.github.com>
2019-03-31 18:43:42 -0500

    Add files via upload
    made formula menu TableView also made the menu controller all labels

---
+++ CS345-Final_CalculatorProject-master/Calculator_Project/ViewController.swift
@@ -0,0 +1,109 @@
+//
+//  ViewController.swift
+//  Calculator_Project
+//
+//  Created by Amel, Nukic L on 3/30/19.
+//  Copyright © 2019 Harvell, Tristin L. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSource {
+    let mainMenuLabel: UILabel = UILabel()
+    let formulaHeader: UILabel = UILabel()
+    
+    var tableView = UITableView()
+    var tableData = ["Discount\n" , " Interest\n", " Tips\n", " SalesTax\n"]
+    var tableData2 = ["ListPrice * Discount Price", "Principle * Rate * Time","Cost * Tip Percentage Rate", "Price Of Item * Tax Rate"]
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        tableView.reloadData()
+        
+        
+        //screen constants
+        let screenSize: CGSize = UIScreen.main.bounds.size
+        let centerX: CGFloat = screenSize.width / 2
+        let _: CGFloat = screenSize.height / 2
+        
+        //assign the first tenth of the screen's height to the menu
+        let formulaHeaderSection: CGFloat = screenSize.height/10
+        
+        //button constants
+        let buttonHeight: CGFloat = screenSize.height/7
+        let buttonWidth: CGFloat = screenSize.width-10
+        let buttonX: CGFloat = 5
+        
+        tableView = UITableView(frame: self.view.bounds, style: UITableView.Style.plain)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.backgroundColor = UIColor.white
+        tableView.rowHeight = 100
+        
+        
+
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "my")
+        view.addSubview(tableView)
+        
+        //main menuLabel label
+        mainMenuLabel.text = "Main Menu"
+        mainMenuLabel.backgroundColor = UIColor.darkGray
+        mainMenuLabel.textColor = UIColor.white
+        mainMenuLabel.textAlignment = NSTextAlignment.center
+        mainMenuLabel.frame = CGRect(x: buttonX, y: buttonHeight*6-25, width: buttonWidth, height: buttonHeight)
+        mainMenuLabel.isUserInteractionEnabled = true
+        mainMenuLabel.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(FormulaMenuController.openMainMenu(_:))))
+        self.view.addSubview(mainMenuLabel)
+        
+        self.view.backgroundColor = UIColor.lightGray
+        
+        //previous data menu header
+        formulaHeader.attributedText = NSAttributedString(string: "Favorite Formulas", attributes:
+            [.underlineStyle: NSUnderlineStyle.single.rawValue])
+        formulaHeader.font = UIFont.systemFont(ofSize: 22)
+        formulaHeader.textColor = UIColor.black
+        formulaHeader.textAlignment = NSTextAlignment.center
+        formulaHeader.frame = CGRect(x: centerX/2, y: formulaHeaderSection/2, width: centerX, height: formulaHeaderSection)
+        self.view.addSubview(formulaHeader)
+        
+       
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "my", for: indexPath)
+        cell.textLabel?.text = " \(tableData[indexPath.row]) \(tableData2[indexPath.row])"
+        cell.textLabel!.numberOfLines = 0
+        
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    
+    //method to re-open main menu
+    @objc func openMainMenu(_ recognizer: UITapGestureRecognizer) {
+        presentingViewController?.dismiss(animated: true, completion: {
+            () -> Void in
+        })
+    }
+    
+    // method to allow multiple lines in table cell
+    func allowMultipleLines(tableViewCell:UITableViewCell) {
+        tableViewCell.textLabel?.numberOfLines = 2
+        tableViewCell.textLabel?.lineBreakMode = NSLineBreakMode.byWordWrapping
+    }
+    
+}
+    
+
+
---
+++ CS345-Final_CalculatorProject-master/README.md
@@ -0,0 +1,46 @@
+# CS345-Final_CalculatorProject
+
+Short description: A small calculator application. The calculator is intended to minimize the number of buttons, while also incorporating gestures for various functions, such as:
+•	Equals: swipe up
+•	Add: swipe right
+•	Subtract swipe left
+•	Clear: swipe down
+There will also be a “history” display option that allows the user to view previously entered calculations, as usually seen with standard graphing calculators.
+
+3+ drawings:
+
+
+How to fulfill 9 criteria:
+1.	Images, including an app icon
+a.	A calculator app icon
+b.	A calculator logo incorporated within the main menu
+2.	A minimum of 3 interfaces (which are controlled by separate view controllers)
+a.	A basic main menu interface that is displayed when the user opens the app. This will display the option to either perform calculations or to view previous calculation data.
+b.	A calculation display that will show the output, the buttons for the numbers, and a little “touchpad” area that allows the user to interact with by using gestures to perform the calculation functions.
+c.	An interface that shows the previous calculations that have been performed. There is the option to clear the data, otherwise the data should be persistent when the app is terminated.
+d.	An interface that allows one to view and add common formulas. This is also persistent.
+3.	At least two different gestures
+a.	Equals: swipe up
+b.	Add: swipe right
+c.	Subtract swipe left
+d.	Clear: swipe down
+e.	Tap: return back to the main menu
+4.	At least one animation
+a.	Sliding animations that are utilized when switching to different interfaces
+5.	At least one alert
+a.	When adding formulas, an alert pops up to notify the user to enter the formula within a text-field.
+6.	You must obtain text input from the user via a text-field (which may be included in the alert)
+a.	When adding formulas, an alert pops up to notify the user to enter the formula within a text-field.
+7.	You must save some part of the state of your app when the app is terminated and restore it the next time the app is launched
+a.	Previous calculation data will be saved
+b.	Common formulas will be saved
+8.	You must have at least one of the following views:
+a.	Table view
+b.	Scroll view
+i.	The formula interface will be pre-populated with enough formulas to allow a scroll view to work.
+9.	You must have at least one of the following views:
+a.	Button
+i.	Used to navigate to different interfaces
+ii.	Used to choose the numbers that are desired for input
+b.	Slider
+c.	Picker
